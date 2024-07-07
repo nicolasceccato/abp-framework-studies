355bdab9826aa3de216696ac172ad544580e3c20
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-07-07 15:56:22 -0300

    ToDo App backend

---
+++ aspnet-core/src/TodoApp.Application/TodoAppService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace TodoApp
+{
+    public class TodoAppService : ApplicationService, ITodoAppService
+    {
+
+        private readonly IRepository<TodoItem, Guid> _repository;
+
+        public TodoAppService(IRepository<TodoItem, Guid> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<TodoItemDto> CreateAsync(string text)
+        {
+            var todoItem = await _repository.InsertAsync
+            (
+                new TodoItem { Text = text }
+            );
+
+            return new TodoItemDto
+            {
+                Id = todoItem.Id,
+                text = todoItem.Text
+            };
+
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            await _repository.DeleteAsync(id);
+        }
+
+        public async Task<List<TodoItemDto>> GetListAsync()
+        {
+            var items = await _repository.GetListAsync();
+            return items.Select(item => new TodoItemDto
+            {
+                Id = item.Id,
+                text = item.Text
+            }).ToList();
+        }
+    }
+}
